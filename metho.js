dd3f213ec460b87ac7f0a90065dc2a31d234abfc
Eduardo Grajales Villanueva <edgravill@gmail.com>
2022-01-09 21:01:49 -0600

    Set function name as Symbol Description

--- metho.js
+++ metho.js
@@ -1,38 +1,35 @@
-export function add(target, f, outerSyntax=false){
-  return f.length ?
-    outerSyntax ?
-      addProperty(target, f)
-    :
-      addWithParams(target, f)
-  :
-    addSimple(target, f)
+export function add(target, f, outerSyntax = false) {
+  return f.length
+    ? outerSyntax
+      ? addProperty(target, f)
+      : addWithParams(target, f)
+    : addSimple(target, f);
 }
 
 export function addProperty(target, f) {
-  const s = Symbol()
-  target[s] = f
-  return s
+  const s = Symbol(f.name);
+  target[s] = f;
+  return s;
 }
 
 export function addWithParams(target, f) {
-  return(function(...args) {
-    const s = Symbol()
+  return function (...args) {
+    const s = Symbol(f.name);
     Object.defineProperty(target, s, {
       configurable: true,
-      get: function() {
-        delete target[s]
-        return f.apply(this, args)
-      }
-    })
-    return s
-  })
+      get: function () {
+        delete target[s];
+        return f.apply(this, args);
+      },
+    });
+    return s;
+  };
 }
 
 export function addSimple(target, f) {
-  const s = Symbol()
-  Object.defineProperty(target, s, { configurable:true, get: f})
-  return s
+  const s = Symbol(f.name);
+  Object.defineProperty(target, s, { configurable: true, get: f });
+  return s;
 }
-
 
 // may well need the ability to add descriptions for these Symbols that are flying around
