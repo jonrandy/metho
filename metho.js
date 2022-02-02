1aa497addb38a36d5f7baccec6b191c91387e557
Jon Randy <jonrandy@gmail.com>
2022-02-02 14:07:09 +0700

    Adds basic registration

--- metho.js
+++ metho.js
@@ -15,6 +15,7 @@ export function add(targetOrTargets, f, {outerSyntax=false, symbolName=f?.name, 
 export function addProperty(targetOrTargets, f, {symbolName = f?.name, register=false}={}) {
   const s = Symbol(symbolName)
   sanitiseTargets(targetOrTargets).forEach(target => target[s] = f)
+  register && addToRegister(symbolName, s)
   return s
 }
 
@@ -37,14 +38,18 @@ export function addWithParams(targetOrTargets, f, {symbolName = f?.name, registe
     }
   }
   buildTempMethod[methodName].targets = sanitiseTargets(targetOrTargets)
+  register && addToRegister(symbolName, buildTempMethod[methodName])
   return buildTempMethod[methodName]
 }
 
 export function addSimple(targetOrTargets, f, {symbolName = f?.name, register=false}={}) {
   const s = Symbol(symbolName)
   sanitiseTargets(targetOrTargets).forEach(target => Object.defineProperty(target, s, { configurable:true, get: f}))
+  register && addToRegister(symbolName, s)
   return s
 }
 
 const sanitiseTargets = targets => Array.isArray(targets) ? targets : [targets]
+
+const addToRegister = (name, item) => registry[name] = item
 
