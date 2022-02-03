44f982bb8910316a85280b91b6b6ed9b841c32d9
Jon Randy <jonrandy@gmail.com>
2022-02-03 17:59:37 +0700

    Prettify code + add 'useSymbol' option

--- metho.js
+++ metho.js
@@ -1,26 +1,41 @@
-export const data = Symbol('methoData')
+export const data = Symbol("methoData")
 
 const registry = {}
 
-export function add(targetOrTargets, f, {outerSyntax=false, symbolName=f?.name, register=false}={}){
-  return f.length ?
-    outerSyntax ?
-      addProperty(targetOrTargets, f, {symbolName, register})
-    :
-      addWithParams(targetOrTargets, f, {symbolName, register})
-  :
-    addSimple(targetOrTargets, f, {symbolName, register})
+export function add(
+  targetOrTargets,
+  f,
+  {
+    outerSyntax = false,
+    symbolName = f?.name,
+    register = false,
+    useSymbol = false,
+  } = {}
+) {
+  return f.length
+    ? outerSyntax
+      ? addProperty(targetOrTargets, f, { symbolName, register, useSymbol })
+      : addWithParams(targetOrTargets, f, { symbolName, register })
+    : addSimple(targetOrTargets, f, { symbolName, register, useSymbol })
 }
 
-export function addProperty(targetOrTargets, f, {symbolName = f?.name, register=false}={}) {
-  const s = Symbol(symbolName)
-  sanitiseTargets(targetOrTargets).forEach(target => target[s] = f)
+export function addProperty(
+  targetOrTargets,
+  f,
+  { symbolName = f?.name, register = false, useSymbol = false } = {}
+) {
+  const s = useSymbol || Symbol(symbolName)
+  sanitiseTargets(targetOrTargets).forEach(target => (target[s] = f))
   register && addToRegister(symbolName, s)
   return s
 }
 
-export function addWithParams(targetOrTargets, f, {symbolName = f?.name, register=false}={}) {
-  const methodName = symbolName || '__methoIntermediate'
+export function addWithParams(
+  targetOrTargets,
+  f,
+  { symbolName = f?.name, register = false } = {}
+) {
+  const methodName = symbolName || "__methoIntermediate"
   const buildTempMethod = {
     [methodName]: function (...args) {
       const s = Symbol(symbolName)
@@ -28,36 +43,43 @@ export function addWithParams(targetOrTargets, f, {symbolName = f?.name, registe
       targets.forEach(target => {
         Object.defineProperty(target, s, {
           configurable: true,
-          get: function() {
-            targets.forEach(target => { delete target[s] })
+          get: function () {
+            targets.forEach(target => {
+              delete target[s]
+            })
             return f.apply(this, args)
-          }
+          },
         })
       })
       return s
-    }
+    },
   }
   buildTempMethod[methodName].targets = sanitiseTargets(targetOrTargets)
   register && addToRegister(symbolName, buildTempMethod[methodName])
   return buildTempMethod[methodName]
 }
 
-export function addSimple(targetOrTargets, f, {symbolName = f?.name, register=false}={}) {
-  const s = Symbol(symbolName)
-  sanitiseTargets(targetOrTargets).forEach(target => Object.defineProperty(target, s, { configurable:true, get: f}))
+export function addSimple(
+  targetOrTargets,
+  f,
+  { symbolName = f?.name, register = false, useSymbol = false } = {}
+) {
+  const s = useSymbol || Symbol(symbolName)
+  sanitiseTargets(targetOrTargets).forEach(target =>
+    Object.defineProperty(target, s, { configurable: true, get: f })
+  )
   register && addToRegister(symbolName, s)
   return s
 }
 
 export const registered = name => registry[name]
 
-const sanitiseTargets = targets => Array.isArray(targets) ? targets : [targets]
-const addToRegister = (name, item) => registry[name] = item
+const sanitiseTargets = targets => [targets].flat()
 
+const addToRegister = (name, item) => (registry[name] = item)
 
 // TODO - see below for stuff necessary for upcoming method 'name' sharing between metho-string and metho-array,
 // whilst retaining ability for each module to be use separately without polluting the other's target prototype
-
 
 // add a 'useSymbol' option - will use given symbol if specified, instead of creating new one
 
@@ -68,5 +90,3 @@ const addToRegister = (name, item) => registry[name] = item
 //   the new target (maybe using this[Metho.data]), so simply change the registered
 //   function's targets to include new one
 // * otherwise, attach use Metho.add appropriately, including {useSymbol:registeredSymbol} in options
-
-
