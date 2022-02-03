5c66641bb6e179a4c587146ad8e5de324050cd6e
Jon Randy <jonrandy@gmail.com>
2022-02-03 18:12:01 +0700

    Tweak prettier settings to use tabs instead of spaces

--- metho.js
+++ metho.js
@@ -3,73 +3,73 @@ export const data = Symbol("methoData")
 const registry = {}
 
 export function add(
-  targetOrTargets,
-  f,
-  {
-    outerSyntax = false,
-    symbolName = f?.name,
-    register = false,
-    useSymbol = false,
-  } = {}
+	targetOrTargets,
+	f,
+	{
+		outerSyntax = false,
+		symbolName = f?.name,
+		register = false,
+		useSymbol = false,
+	} = {}
 ) {
-  return f.length
-    ? outerSyntax
-      ? addProperty(targetOrTargets, f, { symbolName, register, useSymbol })
-      : addWithParams(targetOrTargets, f, { symbolName, register })
-    : addSimple(targetOrTargets, f, { symbolName, register, useSymbol })
+	return f.length
+		? outerSyntax
+			? addProperty(targetOrTargets, f, { symbolName, register, useSymbol })
+			: addWithParams(targetOrTargets, f, { symbolName, register })
+		: addSimple(targetOrTargets, f, { symbolName, register, useSymbol })
 }
 
 export function addProperty(
-  targetOrTargets,
-  f,
-  { symbolName = f?.name, register = false, useSymbol = false } = {}
+	targetOrTargets,
+	f,
+	{ symbolName = f?.name, register = false, useSymbol = false } = {}
 ) {
-  const s = useSymbol || Symbol(symbolName)
-  sanitiseTargets(targetOrTargets).forEach(target => (target[s] = f))
-  register && addToRegister(symbolName, s)
-  return s
+	const s = useSymbol || Symbol(symbolName)
+	sanitiseTargets(targetOrTargets).forEach(target => (target[s] = f))
+	register && addToRegister(symbolName, s)
+	return s
 }
 
 export function addWithParams(
-  targetOrTargets,
-  f,
-  { symbolName = f?.name, register = false } = {}
+	targetOrTargets,
+	f,
+	{ symbolName = f?.name, register = false } = {}
 ) {
-  const methodName = symbolName || "__methoIntermediate"
-  const buildTempMethod = {
-    [methodName]: function (...args) {
-      const s = Symbol(symbolName)
-      const targets = buildTempMethod[methodName].targets
-      targets.forEach(target => {
-        Object.defineProperty(target, s, {
-          configurable: true,
-          get: function () {
-            targets.forEach(target => {
-              delete target[s]
-            })
-            return f.apply(this, args)
-          },
-        })
-      })
-      return s
-    },
-  }
-  buildTempMethod[methodName].targets = sanitiseTargets(targetOrTargets)
-  register && addToRegister(symbolName, buildTempMethod[methodName])
-  return buildTempMethod[methodName]
+	const methodName = symbolName || "__methoIntermediate"
+	const buildTempMethod = {
+		[methodName]: function (...args) {
+			const s = Symbol(symbolName)
+			const targets = buildTempMethod[methodName].targets
+			targets.forEach(target => {
+				Object.defineProperty(target, s, {
+					configurable: true,
+					get: function () {
+						targets.forEach(target => {
+							delete target[s]
+						})
+						return f.apply(this, args)
+					},
+				})
+			})
+			return s
+		},
+	}
+	buildTempMethod[methodName].targets = sanitiseTargets(targetOrTargets)
+	register && addToRegister(symbolName, buildTempMethod[methodName])
+	return buildTempMethod[methodName]
 }
 
 export function addSimple(
-  targetOrTargets,
-  f,
-  { symbolName = f?.name, register = false, useSymbol = false } = {}
+	targetOrTargets,
+	f,
+	{ symbolName = f?.name, register = false, useSymbol = false } = {}
 ) {
-  const s = useSymbol || Symbol(symbolName)
-  sanitiseTargets(targetOrTargets).forEach(target =>
-    Object.defineProperty(target, s, { configurable: true, get: f })
-  )
-  register && addToRegister(symbolName, s)
-  return s
+	const s = useSymbol || Symbol(symbolName)
+	sanitiseTargets(targetOrTargets).forEach(target =>
+		Object.defineProperty(target, s, { configurable: true, get: f })
+	)
+	register && addToRegister(symbolName, s)
+	return s
 }
 
 export const registered = name => registry[name]
